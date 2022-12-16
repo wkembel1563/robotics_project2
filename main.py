661ad04cf5ea5e13c52c6a914fd8e5094b00d9f3
Will Kembel <wkembel@hotmail.com>
2022-12-15 22:03:49 -0600

    added imports

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env pybricks-micropython
-from bbot import *
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+import math
 
 # Create your objects here.
 ev3 = EV3Brick()
